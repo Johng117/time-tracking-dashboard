b7d0ee124cc16ab02a127f4a245fc6167bd8f3e5
Johng117 <johngorman117@gmail.com>
2025-06-27 21:36:19 +0100

    updates to index.js

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 const timePeriodBtns = document.getElementsByClassName("time-period-button");
+const daily = document.getElementById("daily");
 let dataArray;
 
+function fetchData() {
+  fetch("./data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      dataArray = data;
+      console.log("dataArray:", dataArray);
+    })
+    .catch((err) => console.error(err));
+}
 
-//  function to switch text color of buttons depending on which one is activated
-function logButton(e) {
-  for(btn of timePeriodBtns) {
-    btn.style.color = "hsl(235, 45%, 61%)"
-  }  
-  let activeBtn = document.getElementById(e.target.id);
+// function that fetches the data once the DOM has loaded
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log("DOM fully loaded and parsed");
+  daily.style.color = "white";
+  fetchData();
+});
+
+//  function to switch text color of buttons
+function timePeriodHighlight(timeSpan) {
+  console.log("timeSpan", timeSpan);
+  for (btn of timePeriodBtns) {
+    btn.style.color = "hsl(235, 45%, 61%)";
+  }
+  let activeBtn = document.getElementById(timeSpan);
+  console.log("activeBtn", activeBtn);
   activeBtn.style.color = "white";
 }
 
+function dataFilter(data) {
+   
+  for (obj of dataArray) {
+    console.log(obj["timeframes"][data]);
+  }
+  console.log("data", data);
+}
+
+function switchTimeData(e) {
+  let newTimePeriod = e.target.id;
+  timePeriodHighlight(newTimePeriod);
+  dataFilter(newTimePeriod);
+}
+
 for (btn of timePeriodBtns) {
-  btn.addEventListener("click", logButton);
+  btn.addEventListener("click", switchTimeData);
 }
-
-function fetchData() {
-    fetch("./data.json")
-    .then(res=>res.json())
-    .then((data)=>{
-        dataArray = data;
-        console.log("dataArray:", dataArray);
-    })
-    .catch((err)=>console.error(err));
-}
-
-fetchData();
+console.log("dataArray", dataArray);
